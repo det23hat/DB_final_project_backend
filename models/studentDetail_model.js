0ef7f7ff57b2f20d5fde806b1d32e1a5358a234f
王韻淇 <det23hat@gmail.com>
2020-12-16 16:18:24 +0800

    Modify student-detail dataformat,resolve #1

--- models/studentDetail_model.js
+++ models/studentDetail_model.js
@@ -3,7 +3,7 @@ const db = require('./connection_db');
 module.exports = function studentDetail(sid) {
     let result = {};
     return new Promise((resolve, reject) => {
-        db.query('SELECT stu.user_id, stu.name, stu.department, sum(s.score)/count(distinct s.unit_id) as average, s.unit_id,u.name, s.score FROM units AS u JOIN scores AS s on u.id=s.unit_id JOIN students AS stu ON stu.user_id = s.user_id WHERE stu.user_id = ?',
+        db.query('SELECT stu.user_id as studentID, stu.name as studentName, stu.department, sum(s.score)/count(distinct s.unit_id) as average FROM units AS u JOIN scores AS s on u.id=s.unit_id JOIN students AS stu ON stu.user_id = s.user_id WHERE stu.user_id = ?',
             sid,
             function (err, rows) {
                 if (err) {
@@ -13,8 +13,30 @@ module.exports = function studentDetail(sid) {
                     reject(result);
                     return;
                 }
-                result.status = 'success';
-                resolve(rows);
+                else{
+                    let studentInfo = rows;
+                    console.log(studentInfo);
+                    db.query('SELECT  s.unit_id as unit,u.name as title, s.score FROM units AS u JOIN scores AS s on u.id=s.unit_id JOIN students AS stu ON stu.user_id = s.user_id WHERE stu.user_id = ?',
+                    sid,
+                    function(err, rows){
+                        if (err) {
+                            console.log(err);
+                            result.status = 'fail';
+                            result.err = '伺服器錯誤，請稍後在試！';
+                            reject(result);
+                            return;
+                        }
+                        else{
+                            console.log(rows);
+                            studentInfo[0].scores = rows;
+                            result.status = 'success';
+                            resolve(studentInfo[0]);
+                        }
+                    }
+                    )
+                    
+                }
+                
             }
         );
     });
