de2476cf909f95c7c1bd3c430e042ef67e2aa6a2
王韻淇 <det23hat@gmail.com>
2020-12-18 19:05:22 +0800

    modify question overview

--- models/QuestionOverview_model.js
+++ models/QuestionOverview_model.js
@@ -5,7 +5,7 @@ module.exports = function questionOverview() {
     let result = {};
     return new Promise((resolve, reject) => {
 
-        db.query('SELECT u.id , u.name, avg(s.score) as unitAvg FROM units AS u JOIN scores AS s ON s.unit_id = u.id GROUP BY u.id having unitAvg >80 ORDER BY u.id',
+        db.query('SELECT  u.id, u.name, avg(s.score) as unitAvg FROM units AS u JOIN scores AS s ON s.unit_id = u.id GROUP BY u.id having count(s.score)=(SELECT count(*) FROM students) AND  unitAvg >80 ORDER BY u.id',
         function(err,easyRows){
             if (err) {
                 console.log(err);
@@ -16,7 +16,7 @@ module.exports = function questionOverview() {
             }
             result.easy = easyRows;
             console.log(easyRows[0]);
-            db.query('SELECT u.id, u.name, avg(s.score) as unitAvg FROM units AS u JOIN scores AS s ON s.unit_id = u.id GROUP BY u.id having unitAvg >=60 AND unitAvg <=80 ORDER BY u.id',
+            db.query('SELECT  u.id, u.name, avg(s.score) as unitAvg FROM units AS u JOIN scores AS s ON s.unit_id = u.id GROUP BY u.id having count(s.score)=(SELECT count(*) FROM students) AND unitAvg >=60 AND unitAvg <=80 ORDER BY u.id',
             function(err,middleRows){
                 if (err) {
                     console.log(err);
@@ -27,7 +27,7 @@ module.exports = function questionOverview() {
                 }
                 result.middle = middleRows;
                 console.log(middleRows[0]);
-                db.query('SELECT u.id, u.name, avg(s.score) as unitAvg FROM units AS u JOIN scores AS s ON s.unit_id = u.id GROUP BY u.id  having unitAvg <60 ORDER BY u.id',
+                db.query('SELECT  u.id, u.name, avg(s.score) as unitAvg FROM units AS u JOIN scores AS s ON s.unit_id = u.id GROUP BY u.id having count(s.score)=(SELECT count(*) FROM students) AND unitAvg <60 ORDER BY u.id',
                 function(err,difficultRows){
                     if (err) {
                         console.log(err);
